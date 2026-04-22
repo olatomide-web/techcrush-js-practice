4df9a169fb0aeb7a4b7c88109635ef0ef68584c9
Olatomide Aborisade <joshboris2020@gmail.com>
2026-04-22 07:53:38 +0100

    Update question7.js

--- question7.js
+++ question7.js
@@ -1,10 +1,14 @@
 //Write a function formatName(firstName, lastName) that trims extra spaces from both inputs and returns the full name in this exact format: 'LASTNAME, Firstname'.
 //Test: formatName('  esther  ', '  ogbu  ') should return 'OGBU, Esther'
 
-function formatName(firstName, lastName){
-    let trimmedFirstname =  firstName.trim();
-    let trimmedLastname =  lastName.toUpperCase();
-
-    return `${trimmedFirstname}, ${lastName}`
+function formatName(firstName, lastName) {
+    
+    const trimmedFirst = firstName.trim();
+    const trimmedLast = lastName.trim();
+    
+    const formattedLast = trimmedLast.toUpperCase();
+    const formattedFirst = trimmedFirst.charAt(0).toUpperCase() + trimmedFirst.slice(1).toLowerCase();
+    
+    return `${formattedLast}, ${formattedFirst}`;
 }
-console.log(formatName( "olatomide", "aborisade"))
+console.log(formatName("  olatomide  ", "  ABORISADE  "));    
