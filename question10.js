7c080ee8ce2b6b4d377185e3ea26b7ab455a4e42
Olatomide Aborisade <joshboris2020@gmail.com>
2026-04-22 08:05:10 +0100

    Update question10.js

--- question10.js
+++ question10.js
@@ -1,8 +1,38 @@
 //BONUS: Write a function called summarizeStudent that accepts an array of student objects (each with name and score). The function should return a new array of strings in this format:
 //'Amara scored 88 — Grade: B'
-function summarizeStudent(){
-    const student = {
-        name : "Aborisade Olatomide",
-        score : 98,
+function summarizeStudent(students){
+
+    function gradeScore(score){
+    if (score > 70 && score <= 100){
+        return "A";
+    }
+    else if(score >= 60 && score <= 69){
+        return "B";
+    } 
+    else if(score >= 50 && score <= 59){
+        return "C";
+    } 
+    else if(score >= 40 && score <= 49){
+        return "D";
+    } 
+    else if(score < 40){
+        return "F";
+    }
+    else{
+        return "invalid";
     }
 }
+
+    return students.map(student => {
+        const grade = gradeScore(student.score);
+        return `${student.name} scored ${student.score} — Grade: ${grade}`;
+    });
+
+}
+const studentArray = [
+    { name: "Amara", score: 88 },
+    { name: "Olatomide", score: 62 },
+];
+
+const result = summarizeStudent(studentArray);
+console.log(result);
